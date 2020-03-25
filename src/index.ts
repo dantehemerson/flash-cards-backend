e51b3e39e57671f614e807358c6c968d631d6582
Dante Calderon <mail@dantecalderon.dev>
2020-03-25 00:49:03 -0500

    Send server info in a static file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import express, { Application } from 'express'
 
 const app: Application = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/static', express.static('static'))
+
 app.use('/', (req, res) => {
   res.send('Hello World')
 })
