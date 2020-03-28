fd68dea172b745c852c4cb610ac260784dedd12c
Dante Calderon <mail@dantecalderon.dev>
2020-03-28 18:17:15 -0500

    Add limit and skip options

--- src/utils/pagination.utils.ts
+++ src/utils/pagination.utils.ts
@@ -1,13 +1,15 @@
 import { PaginationQuery } from 'src/types/pagination.types'
 
 const defaults = {
-  search: term => `"%${term}%"`
+  search: (term: string, key: string) => `${key} LIKE "%${term}%"`,
+  limit: (limit: number) => `LIMIT ${limit}`,
+  skip: (skip: number) => `OFFSET ${skip}`
 }
 
 export function parsePaginationOptions(options: PaginationQuery, applyKeys) {
   return Object.keys(options).reduce((query, key) => {
     if (defaults[key]) {
-      return query + applyKeys[key] + ' LIKE ' + defaults[key](options[key])
+      return `${query} ${defaults[key](options[key], applyKeys[key])}`
     }
   }, '')
 }
