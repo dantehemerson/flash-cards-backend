7d8fe07de7519d434006e3c1c77685d6ae188c1e
Dante Calderon <mail@dantecalderon.dev>
2020-03-28 02:17:39 -0500

    Add model

--- src/mysql/connection.ts
+++ src/mysql/connection.ts
@@ -1,16 +1,8 @@
 import mysql from 'mysql'
 
-const connection = mysql.createConnection({
+export const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root',
-  database: 'sql_store'
-})
-
-connection.connect(error => {
-  if (error) {
-    console.error('Error connecting to MySQL ', error)
-  }
-
-  console.error('Connected to MySQL')
+  database: 'flash_cards'
 })
